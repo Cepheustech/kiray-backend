710cbcb3f3b192c562681af57bb577545ba1ed33
Eyob <eyoblema19926@gmail.com>
2025-11-30 22:45:34 +0300

    User model defined

---
+++ database/migrations/2025_11_30_193508_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->string('phone')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('user_type', ['personal','professional']);
+            $table->string('broker_idno')->nullable();
+            $table->string('fyda_fan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
